608592399dffe7d8235dc376b7a77c592e87c4d2
Kazuma Arino <hogeika2@gmail.com>
2024-07-23 18:40:25 +0900

    Fix race condition of dataPromise and responseReceived
    This is the cause of "window size reached" bug.
    If responseReceived thread is suspended when dataPromise.succeed call, and another thread which waked up by dataPromisss.succeed call reciveData again,
    that second call create dataPromise promise. Then resume responseRecived thread clear dataPromise property without calling succeed, which cause forever waiting.
    
    In this fix, dataPromise and cachedRecivedData is isolated by actor and access only from the same actor context.

--- Sources/HPRTMP/Network/NetworkClient.swift
+++ Sources/HPRTMP/Network/NetworkClient.swift
@@ -36,14 +36,64 @@ final class RTMPClientHandler: ChannelInboundHandler {
   }
 }
 
+/**
+ Data Reserver. If data is avaialable, use that data exactly once.
+ If data is not available, wait data is comming with promise.
+ When data is comming, client must call dataArrived.
+ */
+actor DataReserver {
+  private var cachedData: Data = .init()
+  private var dataPromise: EventLoopPromise<Data>?
+  
+  /**
+   return cached data if it's not empty.
+   Or return nil if it's empty.
+   after calling this method, cachedData is renewed.
+   */
+  func tryRetrieveCache() -> Data? {
+    if cachedData.isEmpty {
+      return nil
+    } else {
+      let data = cachedData
+      cachedData = Data()
+      return data
+    }
+  }
+  
+  func waitData(with promise: EventLoopPromise<Data>) async throws -> Data {
+    self.dataPromise = promise
+    return try await promise.futureResult.get()
+  }
+  
+  private func dataArrivedInner(data: Data) {
+    cachedData.append(data)
+    if let promise = dataPromise {
+      self.dataPromise = nil
+      let current = tryRetrieveCache()!
+      promise.succeed(current)
+    }
+  }
+  
+  nonisolated func dataArrived(data: Data) {
+    Task {
+      await dataArrivedInner(data: data)
+    }
+  }
+  
+  func close() {
+    dataPromise?.fail(NSError(domain: "RTMPClientError", code: -2, userInfo: [NSLocalizedDescriptionKey: "Connection invalidated"]))
+    dataPromise = nil
+    cachedData = Data()
+  }
+}
+
 class NetworkClient: NetworkConnectable {
   private let group: EventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
   private var channel: Channel?
   private var host: String?
   private var port: Int?
   
-  private var cachedReceivedData: Data = .init()
-  private var dataPromise: EventLoopPromise<Data>?
+  private let dataReserver = DataReserver()
   
   private let logger = Logger(subsystem: "HPRTMP", category: "NetworkClient")
 
@@ -91,58 +141,22 @@ class NetworkClient: NetworkConnectable {
     guard let channel = self.channel else {
       throw NSError(domain: "RTMPClientError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Connection not established"])
     }
-    if !cachedReceivedData.isEmpty {
-      logger.debug("[HPRTMP] use cachedReceivedData")
-      let data = cachedReceivedData
-      cachedReceivedData = Data()
-      return data
-    }
-
-    /*
-    let promise = channel.eventLoop.makePromise(of: Data.self)
-    self.dataPromise = promise
-    return try await promise.futureResult.get()
-     */
     
-    /*
-    self.dataPromise = channel.eventLoop.makePromise(of: Data.self)
-    return try await dataPromise!.futureResult.get()
-     */
-    self.dataPromise = channel.eventLoop.makePromise(of: Data.self)
-    // --- dataPromise might be cleard in responseReceived between above line and below line. ----
-    logger.debug("[HPRTMP] before future get")
-    let ret = try await dataPromise?.futureResult.get()
-    guard let ret else {
-      print("DEBUG!!!!! race condition happens!")
-      print("!!!!!!!!!!!!!!!!!!!!!!!")
-      print("!!!!!!!!!!!!!!!!!!!!!!!")
-      print("!!!!!!!!!!!!!!!!!!!!!!!")
-      throw NSError(domain: "RTMPClientError", code: -1, userInfo: [NSLocalizedDescriptionKey: "dataPromise becomes nil: race condition!"])
+    if let cache = await dataReserver.tryRetrieveCache() {
+      return cache
     }
-    logger.debug("[HPRTMP] after future get")
-    return ret
+    
+    let waitPromise = channel.eventLoop.makePromise(of: Data.self)
+    return try await dataReserver.waitData(with:waitPromise)
   }
   
   private func responseReceived(data: Data) {
-    logger.debug("[HPRTMP] responseReceived")
-    cachedReceivedData.append(data)
-    if let dataPromise {
-      logger.debug("[HPRTMP] before dataPromise.succeed")
-      dataPromise.succeed(cachedReceivedData)
-      logger.debug("[HPRTMP] after dataPromise.succeed")
-      cachedReceivedData = Data()
-      logger.debug("[HPRTMP] cachedReceivedData cleard")
-      self.dataPromise = nil
-      logger.debug("[HPRTMP] dataPromise cleared")
-    } else {
-      logger.debug("[HPRTMP] dataPromise is nil")
-    }
+    dataReserver.dataArrived(data: data)
   }
   
   func close() async throws {
-    dataPromise?.fail(NSError(domain: "RTMPClientError", code: -2, userInfo: [NSLocalizedDescriptionKey: "Connection invalidated"]))
+    await dataReserver.close()
     let channel = self.channel
-    dataPromise = nil
     self.channel = nil
     try await channel?.close()
   }
